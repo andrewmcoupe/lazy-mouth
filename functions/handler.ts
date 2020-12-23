d83800abd87f51ae55b42a87c8dd2cdb81e01c0f
Andy Coupe <andrewmcoupe@gmail.com>
2020-12-23 15:01:32 +0000

    initial commit

---
+++ functions/handler.ts
@@ -0,0 +1,9 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import 'source-map-support/register';
+
+export const hello: APIGatewayProxyHandler = async () => {
+  return {
+    statusCode: 200,
+    body: 'hello lazy mouth'
+  };
+}
---
+++ serverless.ts
@@ -0,0 +1,54 @@
+import { Serverless } from 'serverless/aws';
+
+const serverlessConfiguration: Serverless = {
+  service: {
+    name: 'lazy-mouth',
+    // app and org for use with dashboard.serverless.com
+    // app: your-app-name,
+    // org: your-org-name,
+  },
+  frameworkVersion: '>=1.72.0',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  // Add the serverless-webpack plugin
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+  },
+  resources: {
+    Resources: {
+      creationsTable: {
+        Properties: {
+          TableName: 'creations_${opt:stage, self:provider.stage, \'dev\'}',
+        },
+        Type: 'AWS::DynamoDB::Table'
+      }
+    }
+  },
+  functions: {
+    hello: {
+      handler: 'functions/handler.hello',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'hello',
+          }
+        }
+      ]
+    }
+  }
+}
+
+module.exports = serverlessConfiguration;
