5ead33e11d1fc2822c072590467ebeadc6de8980
Andy Coupe <andrewmcoupe@gmail.com>
2021-01-05 08:03:36 +0000

    remove get integration

--- src/testing/test-helpers.ts
+++ src/testing/test-helpers.ts
@@ -5,5 +5,5 @@ export const stub = <T>(obj: Partial<T>) => {
 }
 
 export const handleStack = (action: 'deploy' | 'remove', stage: 'prod' | 'dev' | 'integration') => {
-  execSync(`serverless ${action} --stage ${stage}`, { encoding: 'utf8' })
+  execSync(`npx serverless ${action} --stage ${stage}`, { encoding: 'utf8' })
 }
