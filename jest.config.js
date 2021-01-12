79aaaeed6acfd8cca363e3d206018b3810d251a4
Andy Coupe <andrewmcoupe@gmail.com>
2021-01-12 23:15:09 +0000

    add test coverage

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,13 @@ module.exports = {
   preset: 'ts-jest',
   setupFiles: ['<rootDir>/.jest/setEnvVars.js'],
   testEnvironment: 'node',
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.integration.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 }
